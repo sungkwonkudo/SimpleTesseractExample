bb80d412539f4e97aad9f8d6214ed2387506023a
sungkwonkudo <sunries.k@gmail.com>
2017-10-20 22:01:59 -0400

    Works with api 24+,  but not with API 22.

--- app/src/main/java/com/imperialsoupgmail/tesseractexample/MainActivity.java
+++ app/src/main/java/com/imperialsoupgmail/tesseractexample/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     Bitmap image;
     private TessBaseAPI mTess;
     String datapath = "";
-    String LANG = "";
+    String LANG = "jpn";
 
     // Camera
     private static int TAKE_PICTURE = 1;
@@ -58,13 +58,13 @@ public class MainActivity extends AppCompatActivity {
         //image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image);
 
         //initialize Tesseract API
-        LANG = "eng";
+        String language = LANG;
         datapath = getFilesDir()+ "/tesseract/";
         mTess = new TessBaseAPI();
 
         checkFile(new File(datapath + "tessdata/"));
 
-        mTess.init(datapath, LANG);
+        mTess.init(datapath, language);
     }
 
     public String processImage(View view){
